64af2fee9295f231f67a81a5ab9088d6258ae1db
B0N3head <47313866+B0N3head@users.noreply.github.com>
2020-07-10 00:46:10 +1000

    Add files via upload

---
+++ ServerStarter/F1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace MediaDL
+{
+    partial class F1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(F1));
+            this.label5 = new System.Windows.Forms.Label();
+            this.supportedSites = new System.Windows.Forms.Button();
+            this.howWork = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.sourceFiles = new System.Windows.Forms.Button();
+            this.reinstall = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Leelawadee UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.label5.Location = new System.Drawing.Point(7, 4);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(136, 30);
+            this.label5.TabIndex = 98;
+            this.label5.Text = "Da Helpdesk";
+            // 
+            // supportedSites
+            // 
+            this.supportedSites.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(62)))), ((int)(((byte)(70)))));
+            this.supportedSites.FlatAppearance.BorderSize = 0;
+            this.supportedSites.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.supportedSites.Font = new System.Drawing.Font("Leelawadee UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.supportedSites.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.supportedSites.Location = new System.Drawing.Point(9, 44);
+            this.supportedSites.Name = "supportedSites";
+            this.supportedSites.Size = new System.Drawing.Size(150, 25);
+            this.supportedSites.TabIndex = 109;
+            this.supportedSites.Text = "What sites are supported?";
+            this.supportedSites.UseVisualStyleBackColor = false;
+            this.supportedSites.Click += new System.EventHandler(this.supportedSites_Click);
+            // 
+            // howWork
+            // 
+            this.howWork.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(62)))), ((int)(((byte)(70)))));
+            this.howWork.FlatAppearance.BorderSize = 0;
+            this.howWork.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.howWork.Font = new System.Drawing.Font("Leelawadee UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.howWork.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.howWork.Location = new System.Drawing.Point(9, 13);
+            this.howWork.Name = "howWork";
+            this.howWork.Size = new System.Drawing.Size(137, 25);
+            this.howWork.TabIndex = 110;
+            this.howWork.Text = "I still dont understand";
+            this.howWork.UseVisualStyleBackColor = false;
+            this.howWork.Click += new System.EventHandler(this.howWork_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.checkBox1.Font = new System.Drawing.Font("Leelawadee UI", 8.25F);
+            this.checkBox1.ForeColor = System.Drawing.Color.Gray;
+            this.checkBox1.Location = new System.Drawing.Point(7, 14);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(138, 17);
+            this.checkBox1.TabIndex = 111;
+            this.checkBox1.Text = "Check updates for me";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.checkBox3);
+            this.panel1.Controls.Add(this.checkBox2);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Location = new System.Drawing.Point(335, 226);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(206, 78);
+            this.panel1.TabIndex = 112;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.F1_MouseDown);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.checkBox3.Font = new System.Drawing.Font("Leelawadee UI", 8.25F);
+            this.checkBox3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.checkBox3.Location = new System.Drawing.Point(7, 56);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(187, 17);
+            this.checkBox3.TabIndex = 113;
+            this.checkBox3.Text = "Remove the supid faces n\' stuff";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.checkBox3_MouseDown);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.checkBox2.Font = new System.Drawing.Font("Leelawadee UI", 8.25F);
+            this.checkBox2.ForeColor = System.Drawing.Color.Gray;
+            this.checkBox2.Location = new System.Drawing.Point(7, 35);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(195, 17);
+            this.checkBox2.TabIndex = 112;
+            this.checkBox2.Text = "Check updates for dependencies";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Leelawadee UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.label3.Location = new System.Drawing.Point(342, 219);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 13);
+            this.label3.TabIndex = 113;
+            this.label3.Text = "Some Checkboxes";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Leelawadee UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.label4.Location = new System.Drawing.Point(19, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(225, 13);
+            this.label4.TabIndex = 115;
+            this.label4.Text = "How do I download my epic videos/music";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(12, 62);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(528, 153);
+            this.panel2.TabIndex = 114;
+            this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.F1_MouseDown);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Leelawadee UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.label6.Location = new System.Drawing.Point(19, 219);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(97, 13);
+            this.label6.TabIndex = 117;
+            this.label6.Text = "Oh some buttons";
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.sourceFiles);
+            this.panel3.Controls.Add(this.reinstall);
+            this.panel3.Controls.Add(this.supportedSites);
+            this.panel3.Controls.Add(this.howWork);
+            this.panel3.Location = new System.Drawing.Point(12, 226);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(316, 78);
+            this.panel3.TabIndex = 116;
+            this.panel3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.F1_MouseDown);
+            // 
+            // sourceFiles
+            // 
+            this.sourceFiles.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(62)))), ((int)(((byte)(70)))));
+            this.sourceFiles.FlatAppearance.BorderSize = 0;
+            this.sourceFiles.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.sourceFiles.Font = new System.Drawing.Font("Leelawadee UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sourceFiles.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.sourceFiles.Location = new System.Drawing.Point(152, 13);
+            this.sourceFiles.Name = "sourceFiles";
+            this.sourceFiles.Size = new System.Drawing.Size(153, 25);
+            this.sourceFiles.TabIndex = 112;
+            this.sourceFiles.Text = "Where my source files be";
+            this.sourceFiles.UseVisualStyleBackColor = false;
+            this.sourceFiles.Click += new System.EventHandler(this.sourceFiles_Click);
+            // 
+            // reinstall
+            // 
+            this.reinstall.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(62)))), ((int)(((byte)(70)))));
+            this.reinstall.FlatAppearance.BorderSize = 0;
+            this.reinstall.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.reinstall.Font = new System.Drawing.Font("Leelawadee UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reinstall.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.reinstall.Location = new System.Drawing.Point(165, 44);
+            this.reinstall.Name = "reinstall";
+            this.reinstall.Size = new System.Drawing.Size(140, 25);
+            this.reinstall.TabIndex = 111;
+            this.reinstall.Text = "Reinstall Dependencies";
+            this.reinstall.UseVisualStyleBackColor = false;
+            this.reinstall.Click += new System.EventHandler(this.reinstall_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Leelawadee UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.label1.Location = new System.Drawing.Point(9, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 99;
+            this.label1.Text = "And also settings";
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(173)))), ((int)(((byte)(181)))));
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(115)))), ((int)(((byte)(119)))));
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(524, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(16, 16);
+            this.button4.TabIndex = 118;
+            this.button4.Text = "X";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(553, 339);
+            this.panel4.TabIndex = 115;
+            this.panel4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.F1_MouseDown);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MediaDL.Properties.Resources.h1;
+            this.pictureBox1.Location = new System.Drawing.Point(9, 11);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(509, 133);
+            this.pictureBox1.TabIndex = 100;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // F1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(40)))), ((int)(((byte)(49)))));
+            this.ClientSize = new System.Drawing.Size(553, 339);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.panel4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "F1";
+            this.Text = "Helpdesk of The";
+            this.Load += new System.EventHandler(this.F1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.F1_MouseDown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button supportedSites;
+        private System.Windows.Forms.Button howWork;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button sourceFiles;
+        private System.Windows.Forms.Button reinstall;
+        private System.Windows.Forms.Panel panel4;
+    }
+}
